0bade961b442da4142a4d7d91a0ac7c14f736b77
Misbahul Arifin <33975227+msbarf14@users.noreply.github.com>
2022-08-22 02:58:55 +0800

    Build roll up

---
+++ public/build/assets/Label.c04ef1b0.js
@@ -0,0 +1,1 @@
+import{b as e,g as t,l as o,z as l}from"./app.753034a2.js";const n={class:"block font-medium text-sm text-gray-700"},r={key:0},c={key:1},m={__name:"Label",props:{value:String},setup(s){return(a,_)=>(e(),t("label",n,[s.value?(e(),t("span",r,o(s.value),1)):(e(),t("span",c,[l(a.$slots,"default")]))]))}};export{m as _};
